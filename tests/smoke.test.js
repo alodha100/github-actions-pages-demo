bc69e8d5d6eb2691b8ddc10e9ace59e6714df4a1
Ajay <lodhaajay100@gmail.com>
2026-06-30 13:04:13 +0530

    Fix smoke test import

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,5 +1,6 @@
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
+import test from 'node:test';
 
 test('package has build and deployment demo metadata', async () => {
   const packageJson = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf8'));
